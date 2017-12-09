abd03bada2ce67c748c397fbf98eecb12a34b8d1
GustavMalm <gustav.malm94@hotmail.com>
2017-12-09 18:20:09 +0100

    Change of system for CRUD-implementation
    + Method(s) now takes in a Object instead of regular strings.
    
    ! Known Issue(s):
    - The Update method is overloading the registration date of the Object
    and is going to be solved in the next patch.

--- src/main/java/com/jensen/employeeregister/controller/EmployeeController.java
+++ src/main/java/com/jensen/employeeregister/controller/EmployeeController.java
@@ -1,20 +1,12 @@
 package main.java.com.jensen.employeeregister.controller;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-
 import main.java.com.jensen.employeeregister.model.bean.Employee;
 import main.java.com.jensen.employeeregister.model.service.IEmployeeService;
 
@@ -25,75 +17,51 @@ public class EmployeeController {
 	private IEmployeeService employeeService;
 
 	@RequestMapping(value = {"/", "index"})
-	public ModelAndView employee() {
-
-		ModelAndView modelAndView = new ModelAndView("index");
-		modelAndView.addObject("employees",employeeService.getAllEmployees());
-		return modelAndView;
+	public String employee(Model model) {
+		// model.addAttribute("employee", new Employee());
+		model.addAttribute("employees", this.employeeService.getAllEmployees());
+		
+		return "index";
 	}
 
-	@RequestMapping(value = "/getEmployee")
-	public String getEmployee(ModelMap modelMap, HttpServletRequest request) {
-
-		int id = Integer.parseInt(request.getParameter("id"));
-		modelMap.addAttribute("employees",employeeService.getEmployeeById(id));
+	@RequestMapping(value = "/employee/{id}")
+	public String getEmployee(@PathVariable("id") int id, Model model) {
+		model.addAttribute("employee", this.employeeService.getEmployeeById(id));
+		model.addAttribute("employees", this.employeeService.getAllEmployees());
 
 		return "index";
 	}
+	
 	@RequestMapping(value = "/edit/{id}")
 	public String editEmployee(@PathVariable("id") int id, Model model) {
 		model.addAttribute("employee", this.employeeService.getEmployeeById(id));
-		//		model.addAttribute("employees", this.employeeService.getAllEmployees());
-		return "edit";
+		model.addAttribute("employees", this.employeeService.getAllEmployees());
+		
+		return "index";
 	}
 	
-	@RequestMapping(value= "/addEmployee")
-	public String addEmployee(HttpServletRequest request) {
-		
-		String firstname = request.getParameter("firstname");
-		String lastname = request.getParameter("lastname");
-		String location = request.getParameter("location");
-		String role = request.getParameter("role");
-
-		Employee employee = new Employee(firstname, lastname, location, role);
-
-		this.employeeService.addEmployee(employee);
-		
-		return "redirect:/index";
-	}
-	
-	@RequestMapping(value= "/add")
-	public String add() {
-		
-		return "add";	
-	}
-	
-	@RequestMapping(value = "/edit/updateEmployee")
-	public String updateEmployee(HttpServletRequest request) throws ParseException {
-		
-		String id = request.getParameter("id");
-		String firstname = request.getParameter("firstname");
-		String lastname = request.getParameter("lastname");
-		String location = request.getParameter("location");
-		String role = request.getParameter("role");
-
-		String date = request.getParameter("registration_date");
-		SimpleDateFormat simpleDate = new SimpleDateFormat("yyyy-mm-dd");
-
-		Date newdate = simpleDate.parse(date);
-		Employee employee = new Employee(Integer.parseInt(id),firstname, lastname, location, role, newdate);
-
-		this.employeeService.updateEmployee(employee);
+	/* 
+	 * Create new Employee (or if the employee already exists updates that Object reference)
+	 * */
+	@PostMapping(value= "/employee/add")
+	public String addEmployee(@ModelAttribute("employee") Employee employee, Model model) {
+		if(employee.getId() == 0) {
+			this.employeeService.addEmployee(employee);
+			model.addAttribute("employee", new Employee());
+		} else {
+			this.employeeService.updateEmployee(employee);
+		}
 		
 		return "redirect:/index";
 	}
 
+	/* 
+	 * Delete this Employee by an ID-variable
+	 * */
 	@RequestMapping(value = "/delete/{id}")
 	public String deleteEmployee(@PathVariable("id") int id) {
 		this.employeeService.deleteEmployee(id);
 
 		return "index";
 	}
-
-
 }
--- src/main/java/com/jensen/employeeregister/spring/configuration/WebAppConfiguration.java
+++ src/main/java/com/jensen/employeeregister/spring/configuration/WebAppConfiguration.java
@@ -1,12 +1,10 @@
 package main.java.com.jensen.employeeregister.spring.configuration;
 
 
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -27,31 +25,24 @@ public class WebAppConfiguration extends WebMvcConfigurerAdapter {
 		templateResolver.setPrefix("/WEB-INF/view/");
 		templateResolver.setSuffix(".html");
 		templateResolver.setTemplateMode("HTML5");
+		
 		return templateResolver;
 	}
 
 	@Bean(name = "templateEngine")
 	public SpringTemplateEngine getTemplateEngine() {
-
 		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
 		templateEngine.setTemplateResolver(this.getTemplateResolver());
+		
 		return templateEngine;
 	}
 
 	@Bean(name = "viewResolver")
 	public ThymeleafViewResolver getViewResolver() {
-
 		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 		viewResolver.setTemplateEngine(this.getTemplateEngine());
+		
 		return viewResolver;
-	}
-	@Bean(name = "messageSource")
-	public MessageSource getMessageSource() {
-
-		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("/WEB-INF/i18/blogmsg");
-		messageSource.setDefaultEncoding("UTF-8");
-		return messageSource;
 	}
 	
 	@Override
